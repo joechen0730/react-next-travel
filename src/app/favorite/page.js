894e9143b7fa8f3178c25417cbe138a8c6793715
Alex 勛 <yes852956@gmail.com>
2026-03-28 01:53:18 +0800

    Init Project 建立專案 - 設立 .gitigore - 建置 scss 套件

---
+++ src/app/favorite/page.js
@@ -0,0 +1,15 @@
+import'../styles/_button.scss'
+
+function About() {
+  return (
+    <div> 
+      <div class="card">About</div>
+      <a href="/">
+        Home
+      </a>
+    </div>
+
+  )
+}
+
+export default About
